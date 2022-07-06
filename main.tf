a177118ff5a9b5310a2bb0eae633e2de3b250396
ROYROHITH06 <98080097+ROYROHITH06@users.noreply.github.com>
2022-07-06 18:55:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
   #profile = "default"
-  access_key = "AKIA2GAAFSX7UNAANJ4H"
-  secret_key = "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"
+  access_key = "********"
+  secret_key = "*********"
  }
 
 #Create a VPC
